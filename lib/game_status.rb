921748be9733d277333877f5052a0f640b8e93ae
johnsontaiwo <josseytee@gmail.com>
2016-01-06 21:25:07 -0500

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,70 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+def won?(board)
+  
+  WIN_COMBINATIONS.detect do |win_combination|
+  
+   position_taken?(board, win_combination[0]) && board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]]
+    end
+    end
+  
+
+
+def full?(board)
+  board.each do |array|
+    if array == " "
+      return false
+    else 
+      true
+    
+    end
+    end
+  end
+
+def draw?(board)
+
+  if full?(board) && !won?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if full?(board) || won?(board) || draw?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+
+if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+  return "X"
+ elsif
+  board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+  return "O"
+    else
+      nil
+    
+    end
+  end
+  end
+  end
+
+
