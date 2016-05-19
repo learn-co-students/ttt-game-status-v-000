a7608312441cb7867f4f7ecc7edb8cc7798651f7
Adam Cooper <amcooper@gmail.com>
2016-05-19 15:29:14 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,36 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |triad|
+    (board[triad[0]] == "X" || board[triad[0]] == "O") && 
+        board[triad[0]] == board[triad[1]] && board[triad[1]] == board[triad[2]]
+  end
+end
+
+def full?(board)
+  !board.include?(" ")
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  won?(board) ? board[won?(board)[0]] : nil
+end
