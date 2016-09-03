9961a097af2072abdef814ba41e5d8ff754a8087
charlie lee <ctlee9096@gmail.com>
2016-09-03 20:04:46 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,12 +16,16 @@ WIN_COMBINATIONS = [
   # ETC, an array for each win combination#add comma to delineate element of array WIN_COMBINATIONS
 ]
 
-def won?(board) WIN_COMBINATIONS.find do |win|
+def won?(board) WIN_COMBINATIONS.find do |win| #FIND the 1 unique combination in the constant WIN_COMBINATIONS
+  #when its constant argument,win, is assigned to the board array as part of a local variable
+  #that if it passes the logic test of a winning combination, it returns the
+  #winning combination indexes' values
 #binding.pry
     position_1 = board[win[0]]
     position_2 = board[win[1]]
     position_3 = board[win[2]]
-    if position_1 == position_2 && position_2 == position_3 && position_1 != " " #|| position_1 == position_3
+    if position_1 == position_2 && position_2 == position_3 && position_1 != " "
+      #|| position_1 == position_3
          return win
     #elsif position_1 == "X" && position_2 == "O" && position_3 == "X"
         #return true
@@ -29,4 +33,28 @@ def won?(board) WIN_COMBINATIONS.find do |win|
           false
     end
                         end
+end
+
+#accepts a board and returns true if the board has not been won and is full and false if the board is not won and the board is not full,
+#and false if the board is won. You should be able to compose this method solely using the methods you used above with some ruby logic
+def draw?(board)
+  if won?(board)!= true && full?(board)
+    true
+  elsif won?(board) WIN_COMBINATIONS.find {|elem|
+    position_1 = board[win[0]]
+    position_2 = board[win[1]]
+    position_3 = board[win[2]]
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return false
+    end
+    }
+  else
+    false
+  end
+end
+
+def full?(board) #defines the full? method w/board array
+  board.all? { |elem| # iterate through ALL of the board array
+    elem == "X"  || elem == "O" || elem != " "#value is comparable to X OR O OR is not empty
+  }
 end
