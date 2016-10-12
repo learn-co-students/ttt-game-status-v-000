4dc40d74d08ce57313b7a50383a0b2132e94b5ac
Bianca Laurendine <bnlauren@gmail.com>
2016-10-12 04:30:30 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,15 +23,21 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.detect do |pos_nes|
     #board = ["", "", "X", "", "X", "", "X", "", ""]
-    win_index_1 = board[pos_nes[0]] #pos_nes[0] = 2, so win_index_1 = 2
-    win_index_2 = board[pos_nes[1]] #pos_nes[1] = 4, so win_index_2 = 4
-    win_index_3 = board[pos_nes[2]] #pos_nes[2] = 6, so win_index_3 = 6
+
+    def won?(board)
+    WIN_COMBINATIONS.detect do |win_combination|
+      (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") || (board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")
+       end
+ end
+  #  win_index_1 = board[pos_nes[0]] #pos_nes[0] = 2, so win_index_1 = 2
+  #  win_index_2 = board[pos_nes[1]] #pos_nes[1] = 4, so win_index_2 = 4
+#    win_index_3 = board[pos_nes[2]] #pos_nes[2] = 6, so win_index_3 = 6
 
   #  position_1 = board[pos_nes] # load the value of the board at win_index_1 = X
   #  position_2 = board[pos_nes] # load the value of the board at win_index_2 = X
   #  position_3 = board[pos_nes] # load the value of the board at win_index_3 = X
 
-    if win_index_1 == "X" && win_index_2 == "X" && win_index_3 == "X"
+  #  if win_index_1 == "X" && win_index_2 == "X" && win_index_3 == "X"
       return true
        # return the win_combination indexes that won.
     else
