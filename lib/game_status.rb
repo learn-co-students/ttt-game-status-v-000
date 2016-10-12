372ea01265cd8b9d05098b06e710ad3ae89f7078
Bianca Laurendine <bnlauren@gmail.com>
2016-10-12 02:24:33 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,4 +12,29 @@ WIN_COMBINATIONS = [
  [6, 7, 8]
 ]
 
-# Define your WIN_COMBINATIONS constant
+#def display_board(cell)
+#  puts " #x | #x | #x "
+#  puts "-----------"
+#  puts " #x| #o | #x "
+#  puts "-----------"
+#  puts " #o |   | #o "
+#end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |pos_nes|
+    #board = ["", "", "X", "", "X", "", "X", "", ""]
+    win_index_1 = pos_nes[0] #pos_nes[0] = 2, so win_index_1 = 2
+    win_index_2 = pos_nes[1] #pos_nes[1] = 4, so win_index_2 = 4
+    win_index_3 = pos_nes[2] #pos_nes[2] = 6, so win_index_3 = 6
+
+  #  position_1 = board[pos_nes] # load the value of the board at win_index_1 = X
+  #  position_2 = board[pos_nes] # load the value of the board at win_index_2 = X
+  #  position_3 = board[pos_nes] # load the value of the board at win_index_3 = X
+
+    if win_index_1 == "X" && win_index_2 == "X" && win_index_3 == "X"
+      return pos_nes # return the win_combination indexes that won.
+    else
+      false
+    end
+  end
+end
