ccad323bf4ae0352fd31697b9c8f77bfa1ff4d67
Pia Kim <pkim2320@gmail.com>
2016-10-17 20:10:42 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,8 @@ WIN_COMBINATIONS.detect do |win_combination|
       if  board[win_combination[0]] == board[win_combination[1]] &&
         board[win_combination[1]] == board[win_combination[2]] &&
         position_taken?(board, win_combination[0])
-        true
+
+        return win_combination
       else
         false
       end
@@ -28,11 +29,34 @@ WIN_COMBINATIONS.detect do |win_combination|
 end
 
 def full?(board)
+  all_full = true
   board.each do |position|
     if position == "X" || position == "O"
-       true
+       all_full = true
      else
-       false
+       all_full = false
     end
   end
+  all_full
   end
+
+  def draw?(board)
+    if full?(board) == true && !won?(board)
+      true
+    elsif won?(board) == true
+      false
+    else
+      false
+    end
+  end
+
+  def over?(board)
+    won?(board) || draw?(board) || full?(board)
+  end
+
+def winner(board)
+  win_combo = won?(board)
+  if won?(board)
+  return board[win_combo[0]]
+  end
+end
