6a04cff6ac26e686cc1a2aa49a87f8ceab66820c
dwyn-nyc <dwayne.harmon@gmail.com>
2016-10-18 00:56:33 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,9 +1,5 @@
-# Helper Method
+require 'pry'
 
-#   !(board[index].nil? || board[index] == " ")
-# end
-
-# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0, 1, 2], # Top row
   [3, 4, 5], # Middle row
@@ -15,44 +11,44 @@ WIN_COMBINATIONS = [
   [0, 4, 8]  # Diagonal 2
 ]
 
-def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0] #n[0] = 2, so win_index_1 = 2
-    win_index_2 = win_combination[1] #n[1] = 4, so win_index_2 = 4
-    win_index_3 = win_combination[2] #n[2] = 6, so win_index_3 = 6
-#board = ["", "", "X", "", "X", "", "X", "", ""]
-    position_1 = board[win_index_1] # load the value of the board at win_index_1 = X
-    position_2 = board[win_index_2] # load the value of the board at win_index_2 = X
-    position_3 = board[win_index_3] # load the value of the board at win_index_3 = X
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination[0] # return the win_combination indexes that won.
-    else
-      false
-    end
+def won?(board)
+  WIN_COMBINATIONS.detect do |wincombo|
+    board[wincombo[0]] == board[wincombo[1]] &&
+    board[wincombo[1]] == board[wincombo[2]] &&
+    position_taken?(board, wincombo[0])
   end
 end
 
-# for each win_combination in WIN_COMBINATIONS #need to write a loop win_combination = [0,1,2]
-#   # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-#   # grab each index from the win_combination that composes a win.
-#   win_index_1 = win_combination[0] #win_combination[0] = 0, so win_index_1 = 0
-#   win_index_2 = win_combination[1] #win_combination[1] = 1, so win_index_2 = 1
-#   win_index_3 = win_combination[2] #win_combination[2] = 2, so win_index_3 = 2
-#
-#   position_1 = board[0] # load the value of the board at win_index_1 = X
-#   position_2 = board[1] # load the value of the board at win_index_2 = X
-#   position_3 = board[2] # load the value of the board at win_index_3 = X
-#
-#   if position_1 == "X" && position_2 == "X" && position_3 == "X"
-#     return win_combination # return the win_combination indexes that won.
-#   else
-#     false
+# def won?(board)
+#   WIN_COMBINATIONS.detect do |wincombo|
+#     return board[wincombo[0]] == board[wincombo[1]] == board[wincombo[2]]
 #   end
-#
-#
-#
-# if board[WIN_COMBINATIONS[0]] == "X" && board[WIN_COMBINATIONS[1]] == "X" && board[WIN_COMBINATIONS[2]] == "X"
-#   "X won in the top row"
-#   puts WIN_COMBINATIONS[0]
 # end
+
+
+def full?(board)
+  board.all? do |megatron|
+    megatron == "X" || megatron == "O"
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board)
+end
+
+def winner(board)
+  variable = won?(board)
+  if variable
+    return board[wincombo[0]]
+  else
+    return nil
+  end
+end
