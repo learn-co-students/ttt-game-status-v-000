27dc07fe5f931268ee8ab583eb589affdb087b7c
HANA LEANDER <hana0070@gmail.com>
2016-10-26 03:30:33 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,21 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+#won?
+def won?(board)
+  WIN_COMBINATIONS.select do |win_combination|
+    board[win_combinaton[0]] == board[win_combinaton[1]] &&
+    board[win_combinaton[1]] == board[win_combinaton[2]]
+  end
+end
