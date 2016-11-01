861acafc85021e6b159cfcdc1c5d00c5ec6a6668
Camille Jackson <cj@mail.co.de>
2016-11-01 22:53:14 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,35 +16,33 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  result = WIN_COMBINATIONS.select do |combo|
-    if combo[0] == combo[1] && combo[0] == combo[2] && position_taken?(board, combo[0])
+  WIN_COMBINATIONS.any? do |combo|
+    if board[combo[0]] == board[combo[1]] && board[combo[0]] == board[combo[2]] && position_taken?(board, combo[0])
       return combo
     end
-  end
-  if result == []
-    false
-  else
-    result[0]
+    end
+end
+
+def full?(board)
+  board.all? do |index|
+    index != " " && index != nil
   end
 end
 
-#   if WIN_COMBINATIONS[0][0] == WIN_COMBINATIONS[0][1] && WIN_COMBINATIONS[0][0] == WIN_COMBINATIONS[0][2] && position_taken?(board, WIN_COMBINATIONS[0][0])
-#     return WIN_COMBINATIONS[0]
-#   elsif position_taken?(board, WIN_COMBINATIONS[1][0]) && position_taken?(board, WIN_COMBINATIONS[1][1]) && position_taken?(board, WIN_COMBINATIONS[1][2])
-#       return WIN_COMBINATIONS[1]
-#   elsif position_taken?(board, WIN_COMBINATIONS[2][0]) && position_taken?(board, WIN_COMBINATIONS[2][1]) && position_taken?(board, WIN_COMBINATIONS[2][2])
-#       return WIN_COMBINATIONS[2]
-#   elsif position_taken?(board, WIN_COMBINATIONS[3][0]) && position_taken?(board, WIN_COMBINATIONS[3][1]) && position_taken?(board, WIN_COMBINATIONS[3][2])
-#       return WIN_COMBINATIONS[3]
-#   elsif position_taken?(board, WIN_COMBINATIONS[4][0]) && position_taken?(board, WIN_COMBINATIONS[4][1]) && position_taken?(board, WIN_COMBINATIONS[4][2])
-#       return WIN_COMBINATIONS[4]
-#   elsif position_taken?(board, WIN_COMBINATIONS[5][0]) && position_taken?(board, WIN_COMBINATIONS[5][1]) && position_taken?(board, WIN_COMBINATIONS[5][2])
-#       return WIN_COMBINATIONS[5]
-#   elsif position_taken?(board, WIN_COMBINATIONS[6][0]) && position_taken?(board, WIN_COMBINATIONS[6][1]) && position_taken?(board, WIN_COMBINATIONS[6][2])
-#       return WIN_COMBINATIONS[6]
-#   elsif position_taken?(board, WIN_COMBINATIONS[7][0]) && position_taken?(board, WIN_COMBINATIONS[7][1]) && position_taken?(board, WIN_COMBINATIONS[7][2])
-#       return WIN_COMBINATIONS[7]
-#   else
-#     false
-#   end
-# end
+def draw?(board)
+  if won?(board) == false && full?(board) == true
+    return true
+  end
+end
+
+def over?(board)
+  if won?(board) == true || full?(board) == true || draw?(board) == true
+    return true
+  end
+end
+
+def winner(board)
+  if won?(board)
+   board[won?(board)[0]]
+ end
+end
