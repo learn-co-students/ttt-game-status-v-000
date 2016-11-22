8290592c0855419818bc800583b9b36a85486c51
Parker Thompson <park.thompson@gmail.com>
2016-11-22 21:31:59 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 
 # Method to determine which win combination caused a win
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
+  WIN_COMBINATIONS.detect do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -28,9 +28,60 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination[0]
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
     else
       false
     end
   end
+end
+
+#Method to check if the board is full
+def full?(board)
+  board.none? do |index|
+    index == "" || index == " "
+  end
+end
+
+#Method to check for a draw/tie
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  else
+    return false
+  end
+end
+
+#Method to check if game is over
+def over?(board)
+  if won?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+#Method to check who the winner is "X" or "O"
+def winner(board)
+  win_combination = won?(board)
+  if won?(board) == nil
+    return nil
+  end
+  spot_1 = win_combination[0]
+  spot_2 = win_combination[1]
+  spot_3 = win_combination[2]
+
+  spot_1_value = board[spot_1]
+  spot_2_value = board[spot_2]
+  spot_3_value = board[spot_3]
+
+  if spot_1_value == "X" && spot_2_value == "X" && spot_3_value == "X"
+    winner = "X"
+  elsif spot_1_value == "O" && spot_2_value == "O" && spot_3_value == "O"
+    winner = "O"
+  else
+    winner = nil
+  end
+  return winner
 end
