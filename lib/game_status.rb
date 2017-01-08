2996f3811fe0df301f3794627c789bb1e49bda67
junklpm <lance.p.miller@gmail.com>
2017-01-07 23:34:08 -0500

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,25 +19,58 @@ WIN_COMBINATIONS = [
 #-- Helper Methods Below --
 
 def won?(board) #--> want to use a method that returns true or false. .each does not do this. 
-  WIN_COMBINATIONS.detect do |combo|
-  board[combo[0]] == board[combo[1]] &&
-  board[combo[0]] == board[combo[2]] &&
-  board[combo[1]] == board[combo[2]]
-position_taken?(board, index)
-
+  WIN_COMBINATIONS.detect do |combo| #. Combo is a placeholder iterating through a 3 element array, each of which compose a win
+    #take each index, from  placeholder (combo) that composes a win (all of them!)
+  board[combo[0]] == board[combo[1]]&&
+  board[combo[1]] == board[combo[2]]&&
+  position_taken?(board, combo[2]) #combo is an array that contains 3 positions
   end
-false
 end
 
-def full? 
+def full?(board) 
+  board.each do |spot|
+    if spot == " "
+      return false
+    else true
+    end
+  end
 end
 
-def draw?
+
+def draw?(board)
+  !won?(board) && full?(board)
+#literally if the board is full, and not won.
 end
 
-def over?
+def over?(board)
+  won?(board) || full?(board)
 end
 
-def winner
+def winner(board)
+ if new_variable = won?(board)
+  if board[new_variable[2]].include?("X")
+    return "X"
+  else new_variable.include?("O")
+    return "O"
+   end
+  return new_variable
+  #returns [0, 4, 8]
+  end
 end
+
+#capture the array in a variable
+# check if new_variable returns anything... becuase if the game isn't won, you don't want to provide a winner
+
+
+# [0,1,2]
+  #     board = ["X", " ", " ", 
+  # =>           " ", "X", " ", 
+  # =>            " ", " ", "X"]
+
+ #take combo
+ #step through combo, 1 by 1
+ #if combo contains X, X is the winner
+ #if combo contains O, O is the winner
+ #if combo contains x and o, return nil
+ # if combo contains nil, return nil
 
