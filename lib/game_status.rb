88322adde1a6ac6cc78b8a502b182d11bf0c25e3
Trai Compton <tlcompton2546@outlook.com>
2017-01-18 23:04:53 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,35 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # top row
+  [3,4,5], # middle row
+  [6,7,8], # bottom lower
+  [0,3,6], # left column
+  [1,4,7], # middle column
+  [2,5,8], # right column
+  [0,4,8], # diagonal
+  [2,4,6] # diagonal 2
+]
+
+#WON?
+def won?(board)
+  WIN_COMBINATIONS.select do |win_combination|
+      if board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && position_taken?(board, win_combination[0])
+        return win_combination
+      else
+        return false
+      end
+  end
+end
+
+#FULL?
+def full?(board)
+  board.all? do |index|
+    if position_taken?(board, index)
+      return true
+    else
+      return false
+    end
+  end
+end
