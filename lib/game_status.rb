33604e3a75cd368f0c14a69148b867d1802db8e8
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-03-08 22:30:29 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,13 +18,20 @@ WIN_COMBINATIONS= [
 def won?(board)
     WIN_COMBINATIONS.detect do |combination|
       board[combination[0]] == board[combination[1]] && board[combination[1]] == board[combination[2]] && position_taken?(board,combination[0])
+      #this iterates over WIN_COMBINATIONS comparing the 2nd level array postitions of [0,1,2] to see if they all have x's or o's (this returns an array not true or false?
   end   #end for win combination
 end #end for method
 
 def full?(board)
-  board.none? {|space| space == " "}
+  board.none? {|space| space == " "} # this asks if there are any spaces on the board and returns true if full and false if there are spaces
 end
 
 def draw?(board)
-  
+    #  returns true if the board has not been won and is full
+  if full?(board) == true && won?(board) == false
+    return true
+    # and false if the board is not won and the board is not full, and false if the board is won.
+  else won?(board) == false && full?(board) == false  && won?(board) == false
+    return false
+  end
 end
