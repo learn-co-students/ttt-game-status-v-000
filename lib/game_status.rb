7b034bd589aadb6436400f021a536f39f17014a3
Karolina Kumiega <karolina.kumiega@gmail.com>
2017-05-14 23:08:38 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,63 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+#won method
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    if board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X" || board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
+      return combination
+    end
+  end
+  return false
+end
+
+#full board method
+def full?(board)
+  board.each do |index|
+    if index != "X" && index != "O"
+      return false
+    end
+  end
+  return true
+end
+
+#draw method
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  elsif !won?(board) && !full?(board)
+    return false
+  elsif won?(board)
+    return false
+  end
+end
+
+#over method
+def over?(board)
+  if draw?(board) || full?(board) || won?(board)
+    true
+  end
+end
+
+#winner method
+def winner(board)
+  WIN_COMBINATIONS.detect do |combination|
+    if board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X"
+      return "X"
+    elsif board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
+      return "O"
+    end
+  end
+  nil
+end
