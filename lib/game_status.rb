16b8f6c6a3bf01e4555bcd33057f15d80dac13a0
britt <britt.cumens@gmail.com>
2017-06-05 08:22:17 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ def won?(board)
   winning_combo = []
 
   WIN_COMBINATIONS.each do |combo|
-    if board[combo].all? == "X" || "O"
+    if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X" ||  board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
       winning_combo += combo
     end
   end
@@ -29,4 +29,41 @@ def won?(board)
   else
     winning_combo
   end
+end
+
+def full?(board)
+  full = true
+  if board.any?{|space| space == " "}
+    full = false
+  end
+  full
+end
+
+def draw?(board)
+  draw = false
+  if won?(board) == false && full?(board) == true
+    draw = true
+  end
+  draw
+end
+
+def over?(board)
+  over = false
+  if won?(board) != false
+    over = true
+  elsif draw?(board) == true
+    over = true
+  end
+  over
+end
+
+def winner(board)
+  if won?(board).kind_of?(Array)
+    win = won?(board)
+    board[win[0]]
+
+  else
+    nil
+  end
+
 end
