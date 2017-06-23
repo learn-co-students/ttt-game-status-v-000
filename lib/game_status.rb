fb9021db7835b233454dcad2796ee899c68990a1
Steven Lennis <steven.lennis@icloud.com>
2017-06-23 18:08:14 -0500

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,33 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+player_at_position = [ nil, nil, nil, nil, nil, nil, nil, nil, nil]
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+						            [0,1,2],
+						            [3,4,5],
+						            [6,7,8],
+						            [0,3,6],
+						            [1,4,7],
+						            [2,5,8],
+						            [0,4,8],
+						            [6,4,2]
+]
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
-
-# Define your WIN_COMBINATIONS constant
+# Helper Method
+def won?(board)
+  # Helper Method
+  def is_empty?(board)
+    index = 0
+    while index < 9
+      if position_taken?(board, index)
+         index =+ 1
+         puts "Space #{index} taken"
+      end
+      break
+    end
+  end
+  is_empty?(board)
+end
+won?(board)
