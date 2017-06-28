cbd9ad7e1c0e4059ca76d76ac7043d3cd4b0199c
Jerry Boyce <jokerdelta@yahoo.com>
2017-06-28 05:10:54 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,13 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+[0,1,2], # Top Row
+[3,4,5], # Middle Row
+[6,7,8], # Bottom Row
+[0,3,6], # Left Column
+[1,4,7], # Middle Column
+[2,5,8], # Right Column
+[2,4,6], # Diagonal Right Corner
+[0,4,8] # Diagonal Left Corner
+]
