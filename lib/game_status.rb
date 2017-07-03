4b3a92c8fd5d13b56ccbb0ed42fdc0f9ed247d3e
Jerry Boyce <jokerdelta@yahoo.com>
2017-07-03 01:49:44 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,12 +19,19 @@ def won? (board)
 # if there is a win and false/nil if no win combination
 
   WIN_COMBINATIONS.each do |win_combination|
-    win_combination.each do |win_index|
-      if board [win_index[0]] == "X" && board[win_index[1]] == "X" &&
-        board [win_index[2]] == "X
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
 
+    if position_1 == "X" && position_2 == "X" &&
+      position_3 == "X"
+      return win_combination
+    else
+      false
     end
   end
-
 end
