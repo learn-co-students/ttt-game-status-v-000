8900e5c3621512281073ac26b761242f5c2d93e5
Jerry Boyce <jokerdelta@yahoo.com>
2017-07-03 01:54:21 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,7 +29,7 @@ def won? (board)
 
     if position_1 == "X" && position_2 == "X" &&
       position_3 == "X"
-    elseif position_1 == "O" && position_2 == "O" &&
+    elsif position_1 == "O" && position_2 == "O" &&
       position_3 == "O"
       return win_combination
     else
