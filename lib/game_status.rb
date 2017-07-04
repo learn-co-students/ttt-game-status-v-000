9c77ffa73bcb632484a57c6b53f0b14aa849d16b
Jerry Boyce <jokerdelta@yahoo.com>
2017-07-04 16:10:42 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,15 +27,14 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
+    win_player = " "
+
     if position_1 == "X" && position_2 == "X" &&
       position_3 == "X"
       return win_combination
-
-    elsif position_1 == "O" &&
+        elsif position_1 == "O" &&
       position_2 == "O" && position_3 == "O"
       return win_combination
-
-
     end
 
   end
@@ -62,8 +61,8 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board) == true
-    return position_1
+  if won?(board)
+    return board[win_combination[0]]
   else
     return nil
   end
