950e08e0c309244da410d5cfe68125acfeee8e98
Tyler Buchheim <buchheimt@yahoo.com>
2017-07-08 20:31:59 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,41 @@
-# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    if combo.all? {|cell| board[cell] == "X"} || combo.all?{|cell| board[cell] == "O"}
+      return combo
+    end
+  end
+  false
+end
+
+def full?(board)
+  board.all? {|cell| cell == "X" || cell == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  if won?(board)
+    board[won?(board)[0]]
+  end
+end
