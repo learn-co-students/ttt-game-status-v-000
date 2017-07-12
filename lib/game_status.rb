5b6a3a861c2f759eeff46639801c5fa2cd334094
Amanda Wehrman <arwehrman@gmail.com>
2017-07-12 01:37:41 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,7 +13,7 @@ WIN_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
-#define Won method
+#define if there is a winner
 def won?(board)
  WIN_COMBINATIONS.each do |win_combo|
    if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
@@ -23,4 +23,39 @@ def won?(board)
     end
   end
  return false
+end
+
+#define if board is full
+def full?(board)
+  if board.include?(" ") || board.include?("") #empty spaces on board
+    return false
+  else
+    return true
+  end
+end
+
+#define if game is a draw
+def draw?(board)
+  if !won?(board) && full?(board) #no win and the board is full
+    return true
+  end
+end
+
+# define if game is over by Win, Full or draw
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  end
+end
+
+# who is the winner also if there wasn't a winner (nil)
+def winner(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+    if board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X"
+      return "X"
+    elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
+      return "O"
+    else
+    end
+  end
 end
