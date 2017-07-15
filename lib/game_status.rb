98892de2b1ee2464c457ab94f156ce9bfcd15e57
Amberly Armstrong <armstrong.amberly@gmail.com>
2017-07-15 18:44:39 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,59 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+  # grab each index from the win_combination that composes a win.
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+
+  position_1 = board[win_index_1] # load the value of the board at win_index_1
+  position_2 = board[win_index_2] # load the value of the board at win_index_2
+  position_3 = board[win_index_3] # load the value of the board at win_index_3
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return win_combination
+  else
+    false
+  end
+end
+end
+
+
+def full?(board)
+  if
+    board.all? {|element| element == "X" || element == "O"}
+    true
+  elsif board.all? {|element| !element == "X" || element == "O"}
+    false
+end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+ won?(board) || full?(board)
+end
+
+def winner(board)
+  if winning_combination = won?(board)
+    board[winning_combination.first]
+  end
+end
