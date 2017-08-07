a76f0bff3b1b82424511549dd3c91622ec4ace14
Gustavo Villagrana <gusvilla303@gmail.com>
2017-08-07 21:21:06 +0000

    Override work from Saturday with Monday's code

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,16 +1,45 @@
+require 'pry'
+
 # Helper Method
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+  (board[index] == "X" || board[index] == "O") ? true : false
 end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2], #top row
-  [3, 4, 5], #middle row
-  [6, 7, 8], #bottom row
-  [0, 3, 6], #left column
-  [1, 4, 7], #middle column
-  [2, 5, 8], #right column
-  [0, 4, 8], #diagonal top left to bottom right
-  [2, 4, 6] #diagonal top right to bottom left
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    index_1 = combo[0]
+    index_2 = combo[1]
+    index_3 = combo[2]
+
+    position_1 = board[index_1]
+    position_2 = board[index_2]
+    position_3 = board[index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return combo
+    end
+  end
+  return false
+end
+
+
+# Refactor example given my Learn mentor
+# def won?(board)
+#   WIN_COMBINATIONS.detect do |combo|
+#     board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && board[combo[0]] != " "
+#   end
+# end
