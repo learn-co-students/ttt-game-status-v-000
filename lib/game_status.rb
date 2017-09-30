7c0f4a8bc57a49e2ef6256af7b55114cf02f4429
Nganie <ngan.tp.quan@gmail.com>
2017-09-30 15:32:31 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,57 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,4,8], # diagonal right
+  [2,4,6], # diagonal left
+  [0,3,6], # vertical left
+  [1,4,7], # vertical Middle
+  [2,5,8] # vertical right
+]
+
+def won?(board)
+ WIN_COMBINATIONS.each do |win_combination|
+   win_index_1 = win_combination[0]
+   win_index_2 = win_combination[1]
+   win_index_3 = win_combination[2]
+
+   position_1 = board[win_index_1]
+   position_2 = board[win_index_2]
+   position_3 = board[win_index_3]
+
+   if position_1 == "X" && position_2 == "X" && position_3 == "X"
+     return win_combination
+   elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+     return win_combination
+   end
+end
+  false
+end
+
+def full?(board)
+  board.none? { |position| position == " " }
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  if won?(board)
+ position = won?(board)
+ position.each do |win|
+  if board[win] == "X"
+   return "X"
+ else
+   return "O"
+  end
+end
+end
+end
