ed6e7e10c4fa3314f83654d52639f41ce62a3766
Katie Cassell <kcassell944@gmail.com>
2017-10-06 02:04:14 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,15 +3,32 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
- board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+def won?(board)
+  # win_combination is a 3 element ar
+  # grab each index from the win_combination that composes a win.
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
 
-  top_row_win = [0,1,2]
+  position_1 = board[win_index_1] # load the value of the board at win_index_1
+  position_2 = board[win_index_2] # load the value of the board at win_index_2
+  position_3 = board[win_index_3] # load the value of the board at win_index_3
 
-  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board
-    [top_row_win[2]] == "X"
-    "X won in the top row"
-
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination # return the win_combination indexes that won.
+  else
+    false
   end
+end
+# board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+ #
+ #  top_row_win = [0,1,2]
+ #
+ #  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board
+ #    [top_row_win[2]] == "X"
+ #    "X won in the top row"
+ #
+ #  end
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -22,5 +39,3 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [6,4,2]
 ]
- end
-end
