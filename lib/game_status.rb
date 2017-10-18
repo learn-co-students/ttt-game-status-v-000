71709e989060526865c269da2826ad8ef494baac
Connor Gallopo <connor.gallopo@me.com>
2017-10-18 20:07:49 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,34 +15,32 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def won?(board)
-  for WIN_COMBINATIONS.each do |win_combination|
+ def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
-    win_index_4 = win_combination[3]
-    win_index_5 = win_combination[4]
-    win_index_6 = win_combination[5]
-    win_index_7 = win_combination[6]
-    win_index_8 = win_combination[7]
-
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
-  position_4 = board[win_index_4]
-  position_5 = board[win_index_5]
-  position_6 = board[win_index_6]
-  position_7 = board[win_index_7]
-  position_8 = board[win_index_8]
-
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination # return the win_combination indexes that won.
-  elsif position_taken == false
-    false
-  else
 
 
-    false
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+
+    position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      #win_combination # return the win_combination indexes that won.
+
   end
 end
-e
+
+def full?(board)
+  if board.include?(" " || nil)
+    false
+  else
+    true
+  end
+end
+
+def draw?(board)
+full?(board) && !won?(board)
+end
