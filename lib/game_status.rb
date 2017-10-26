5c67e7751221dd657f5787ca513f751e543e9460
Jon Hinson <jonhinsonfx@gmail.com>
2017-10-26 20:08:57 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,26 @@
+require "pry"
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+def won?(board)
+  WIN_COMBINATIONS.each do |condition|
+
+    if board[condition[0]] == "X" && board[condition[1]] == "X" && board[condition[2]] == "X"
+      return condition
+    end
+  end
+  return false
+end
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
