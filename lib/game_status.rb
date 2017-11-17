6d4d64d80f9fd6805d97fc7ab3eb504ed44159d3
Phoebe Lau <phoebelau7@gmail.com>
2017-11-17 02:29:49 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,56 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+def won?(board)
+WIN_COMBINATIONS.any? do |win_combo|
+position_1 = board[win_combo[0]]
+position_2 = board[win_combo[1]]
+position_3 = board[win_combo[2]]
+
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combo
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return win_combo
+  else
+    false
+  end
+end
+end
+
+def full?(board)
+board.none?{|available_spots| available_spots == " " || available_spots == "" || available_spots == nil}
+end
+
+def draw?(board)
+  if won?(board) == false && full?(board) == true
+    true
+  elsif won?(board) == false && full?(board) == false
+    false
+  elsif won?(board) == true
+    false
+  end
+end
+
+def over?(board)
+  if draw?(board) == true || won?(board) != false
+    true
+else
+  false
+end
+end
+
+def winner(board)
+if win_combo = won?(board)
+  return board[win_combo[0]]
+end
+end
