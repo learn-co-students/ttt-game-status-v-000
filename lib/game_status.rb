0e2687cf9c70db50b8ce065cdde1cd1c1f4541d2
neodynia <neodynia@icloud.com>
2017-11-28 19:31:21 -0500

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,5 +18,17 @@ WIN_COMBINATIONS = [
 
 
 def won?(board)
-  WIN_COMBINATIONS.select do |win_combination|
+  WIN_COMBINATIONS.detect do |i|
+    board[i[0]]==board[i[1]] && board[i[0]]==board[i[2]] && position_taken?(board,i[0])
+  end
+end
+
+def full?(board)
+  board.all? do |token|
+    token=="X" || token=="O"
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
 end
