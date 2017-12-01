1abdb716cdc21e0416ab112efe0b8f3c87eb36b9
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-01 19:39:49 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,9 +29,10 @@ def won?(board)
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       winning = win_combination
     end
-
-
   end
 return winning
+end
 
+def full?(board)
+  board.all? {|value| value == "X" || value == "O"}
 end
