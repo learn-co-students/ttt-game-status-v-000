09c7baf87dcb48f022b6ab08fe96b2efdc7fe74a
EPIC448 <samrey2018@gmail.com>
2017-12-05 02:13:47 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,74 @@
-# Helper Method
+#Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+ WIN_COMBINATIONS = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [6,4,2],
+
+ ]
+
+ def won?(board) #['','','X','X','X','X']
+
+   WIN_COMBINATIONS.each do |win_combination|
+      # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+      # grab each index from the win_combination that composes a win.
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      position_1 = board[win_index_1] # load the value of the board at win_index_1
+      position_2 = board[win_index_2] # load the value of the board at win_index_2
+      position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination # return the win_combination indexes that won.
+     elsif  position_1 == "O" && position_2 == "O" && position_3 == "O"
+       return win_combination
+      end
+    end
+    false
+
+ end
+
+
+def full?(board)
+    # are there any? empty spaces on the board
+    # OR are all? of the spaces either "X" or "O"
+    #WIN_COMBINATIONS.all? do |win_combination|
+     board.all? do |element|
+     element == "X" || element == "O"
+
+    end
+
+end
+
+def  draw?(board)
+       full?(board) && !won?(board)
+end
+
+def over?(board)
+    draw?(board) || won?(board)
+
+end
+
+#example
+#board = ["", "", "", X", "X", "X"....]
+#winner1 = [3,4,5]
+ #winner1[0] /3
+ #board[winner1[0]]
+
+
+def winner(board)
+   if winner1 = won?(board)
+     board[winner1[0]]
+   end
+end
