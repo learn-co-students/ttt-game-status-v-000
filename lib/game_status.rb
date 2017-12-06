7389f59c4356dcdc95278cd92e917613ec93951c
Laura <laurao148@gmail.com>
2017-12-06 18:18:02 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -67,11 +67,19 @@ def over?(board)
 end
 
 def winner(board)
-  board.each do |slot|
+  WIN_COMBINATIONS. each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
     if won?(board)
-      if won?(board)[0] == "X" && won?(board)[1] == "X" && won?(board)[2] == "X"
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X")
         return "X"
-      elsif won?(board)[0] == "O" && won?(board)[1] == "O" && won?(board)[2] == "O"
+      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
         return "O"
       end
     else
