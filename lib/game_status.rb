7465bed2c27e91103db4c8e51d008e5e50a9e8bf
Laura <laurao148@gmail.com>
2017-12-06 05:21:36 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,78 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS  = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+
+  WIN_COMBINATIONS. each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      return win_combination
+    else
+      false
+    end
+    end
+
+    if board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      false
+    end
+
+end
+
+def full?(board)
+
+  board.each do |slot|
+    if slot == " "
+      return false
+    end
+  end
+end
+
+def draw?(board)
+  board.each do |slot|
+    if won?(board) == [0,1,2] || won?(board) == [0,4,8] || won?(board) == [2,4,6] || slot == " "
+      return false
+    end
+  end
+end
+
+def over?(board)
+  board.each do |slot|
+    if won?(board)
+      return true
+    elsif slot == " "
+      return false
+    end
+  end
+end
+
+def winner(board)
+  board.each do |slot|
+    if won?(board)
+      if won?(board)[0] == "X" && won?(board)[1] == "X" && won?(board)[2] == "X"
+        return "X"
+      elsif won?(board)[0] == "O" && won?(board)[1] == "O" && won?(board)[2] == "O"
+        return "O"
+      end
+    else
+      return nil
+    end
+  end
+end
