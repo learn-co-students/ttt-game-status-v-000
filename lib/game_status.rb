a93258896a636199e7ad603d247b3742bef014d9
Christopher DiStasio <cadistasio@gmail.com>
2017-12-16 12:41:56 -0600

    Update game_status.rb

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,24 +6,20 @@ WIN_COMBINATIONS = [
   [1,4,7],
   [2,5,8],
   [0,4,8],
-  [6,4,2]  # Middle row
-  # ETC, an array for each win combination
+  [6,4,2] 
 ]
 
-# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
 def won?(board)
   WIN_COMBINATIONS.find {|win_combination| board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && position_taken?(board, win_combination[0])}
 end
 
 def full?(board)
-  # is to check if there are any empty spaces
   board.all? do |position|
-  position == "X" || position =="O" # Will evaluate to true for 1, true for 3
+  position == "X" || position =="O"
 end
 end
 
@@ -37,9 +33,7 @@ def over?(board)
 end
 
 def winner(board)
-  if win_combo = won?(board) # win_combo is equal to [0,1,2]
-    # we have the board that gives us our "X" and "O"
-    # won?(board) is the array returned by the method call
-    return board[win_combo[0]]#FIX LATER
+  if win_combo = won?(board)
+    return board[win_combo[0]]
   end
 end
