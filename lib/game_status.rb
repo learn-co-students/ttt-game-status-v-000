d2218c9b3e4b9e4d2e689cf44392ef0bdcd50777
Kristin Reddington <kristinreddington@gmail.com>
2017-12-19 19:27:05 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,67 @@
-# Helper Method
+require 'pry'
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+[0, 1, 2], [3, 4, 5],
+[6, 7, 8], [0, 4, 8],
+[2, 4, 6], [0, 3, 6],
+[1, 4, 7], [2, 5, 8],
+]
+
+def won?(board)
+
+  WIN_COMBINATIONS.each do |win_index|
+   if board[win_index[0]] == "X" && board[win_index[1]] == "X" && board[win_index[2]] == "X"
+      return win_index.to_ary
+    elsif board[win_index[0]] == "O" && board[win_index[1]] == "O" && board[win_index[2]] == "O"
+      return win_index.to_ary
+    end
+    end
+    false
+end
+
+def full?(board)
+  board.none? { |space|
+if space == " "
+true
+else
+  false
+end
+}
+end
+
+def draw?(board)
+  if won?(board) == false && full?(board) == true
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) && full?(board)
+    true
+  elsif won?(board) && !full?(board)
+    true
+  elsif draw?(board) == true
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if won?(board) == false
+   nil
+ else
+  won?(board).each do |indexes|
+     if board[indexes[0]] == "X"
+      return  "X"
+     elsif board[indexes[0]] == "O"
+       return "O"
+     end
+     end
+end
+end
