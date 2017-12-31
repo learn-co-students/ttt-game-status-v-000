bbf13b95511d01dfad564e0e9a2a2a9fe07868df
Erica Murdock-Waters <ericahpa@gmail.com>
2017-12-31 23:36:46 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,51 @@
 # Helper Method
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+# Define WIN_COMBINATIONS constant with arrays for each win combo
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # LT to RB diagonal
+  [6,4,2] # LB to RT diagonal
+  ]
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+def full?(board)
+  board.all? { |p| p != " " }
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+  board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && position_taken?(board, win_combination[0])
+  end
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    true
+  else !won?(board) && !full?(board)
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if win_combination = won?(board)
+    board[win_combination.first]
+  # else
+  #   nil
+  end
+end
