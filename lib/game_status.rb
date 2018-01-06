83ee90060617893d5f5509067398daacf491cabd
Hanna Vanderloop <hvanderloop@gmail.com>
2018-01-06 21:36:42 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,61 +6,71 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6] # Middle row
+  [3,4,5],  # Middle row
+  [6,7,8], #bottom
+  [0,3,6], #left
+  [1,4,7], #Middle
+  [2,5,8], #right
+  [0,4,8], #left to right
+  [2,4,6] #right to left
 ]
-def won?(board)
-  WIN_COMBINATIONS.detect do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-    position_1 = board[win_index_1] # load the value of the board at win_index_1
-    position_2 = board[win_index_2] # load the value of the board at win_index_2
-    position_3 = board[win_index_3] # load the value of the board at win_index_3
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination # return the win_combination indexes that won.
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
-    else
-      false
-    end
-  end
-  end
 
+def won?(board)
+WIN_COMBINATIONS.detect do |win_combination|
+  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+  # grab each index from the win_combination that composes a win.
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+
+  position_1 = board[win_index_1] # load the value of the board at win_index_1
+  position_2 = board[win_index_2] # load the value of the board at win_index_2
+  position_3 = board[win_index_3] # load the value of the board at win_index_3
+ if position_1 == "X" && position_2 == "X" && position_3 == "X"
+   return win_combination
+ elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+   return win_combination
+else
+  false
+end
+end
+end
+
+#full?
 def full?(board)
   board.none? do |index|
-    index == " "
+  index == " "
 end
 end
 
+#draw?
 def draw?(board)
-  if full?(board) == true && won?(board) == nil
+  if won?(board) == nil && full?(board) == true
     true
-  elsif won?(board) == false && full?(board) == nil
-      false
-  elsif won?(board) != nil
+  elsif won?(board) == false && full?(board) == false
+    false
+  elsif won?(board) != false
     false
   end
 end
 
+#over?
 def over?(board)
-  if won?(board) != nil
+  if draw?(board) == true
     true
-  elsif full?(board) != true
+  elsif won?(board) != nil
+    true
+  elsif
     false
-  elsif draw?(board) == true
-    true
   end
 end
 
+#winner
 def winner(board)
-  if won?(board) != nil
-    win = won?(board)
-    board[win[0]]
-  end
+if won?(board) != nil
+  win = won?(board)
+  board[win[0]]
+else
+  nil
+end
 end
