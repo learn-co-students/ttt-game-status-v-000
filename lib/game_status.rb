a7facc558c134c58f3a5303d5bc7e9134b873514
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-30 12:22:36 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,20 +15,10 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def WON?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X")
-      return win_combination
-    else
-      return false
-    end
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+      board[win_combination[0]] == board[win_combination[1]] &&
+      board[win_combination[1]] == board[win_combination[2]] &&
+      position_taken?(board, win_combination[0]) == true
   end
 end
