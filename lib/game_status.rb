6817d2aefe29ec512bdc9652fb8c48dfbb1f7e82
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-06 21:51:00 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,12 +22,23 @@ end
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+    if (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X")
       return win_combination
-    elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+    elsif (board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")
       return win_combination
     else
-      false
+      return nil
     end
+  end
+end
+
+
+def draw?(board)
+  if won?(board) != false
+    return false
+  elsif full?(board) == false
+    return false
+  else
+    return true
   end
 end
