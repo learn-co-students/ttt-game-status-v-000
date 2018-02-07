cb219e439528193d824cea188a54c8b1479fdb8d
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-07 00:05:04 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,20 +16,44 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def won?(board)
+  if board != [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+   WIN_COMBINATIONS.detect do |combo|
+     if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
+       return combo
+     elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+       return combo
+     else
+       false
+     end
+   end
+end
+end
 
 def full?(board)
   board.none? {|position| position == " "}
 end
 
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
 
-def won?(board)
-  if board != [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-   WIN_COMBINATIONS.each do |combo|
-     if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
-       return combo
-     elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
-       return combo
-     end
-   end
- end
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if combo = won?(board)
+    return board[combo.first]
+  else
+    return nil
+  end
 end
