7eb252b757a3e8d520b250d5ac2f983a3253e60d
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-02-14 04:36:58 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,35 +6,45 @@ end
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
-  [0,1,2], #Top row
-  [3,4,5], #Middle row
-  [6,7,8], #Bottom row
-  [0,3,6], #Left column
-  [1,4,7], #Middle column
-  [2,5,8], #Right column
-  [0,4,8], #Left start diagonal
-  [2,4,6]  #Right start diagonal
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]  
 ]
 
-def won?(board)
-  !array = []
 
-  win_index_1 = win_combination [0]
-  win_index_2 = win_combination [1]
-  win_index_3 = win_combination [2]
-  win_index_4 = win_combination [3]
-  win_index_5 = win_combination [4]
-  win_index_6 = win_combination [5]
-  win_index_7 = win_combination [6]
-  win_index_8 = win_combination [7]
-  win_index_9 = win_combination [9]
-  
-if position_1 == "X" && position_2 == "X" &&      position_3 == "X" && position_4 == "X" &&  position_5 == "X" && position_6 "X" && position_7 == "X" && position_8 "X" && postion_9 == "X"
-  return win_combination
+def won?(board)
+  WIN_COMBINATIONS.each do |win_move|
+  win_index_1 = win_move[0]
+  win_index_2 = win_move[1]
+  win_index_3 = win_move[2]
+ 
+  position_1 = board[win_index_1] 
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3] 
+ 
+  if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return win_move
+  end 
+  end
 else 
   false 
 end 
-end 
+
+
+
+
+
+ 
+   
+
+ 
+
+
  
  
  
