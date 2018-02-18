537003b303891a6f88d6114b463ac266e46ff5c5
Andrew Kairys <andrewkairys@gmail.com>
2018-02-18 17:04:59 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,8 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
+WIN_COMBINATIONS =
+[
   [0,1,2],
   [3,4,5],
   [6,7,8],
@@ -14,39 +15,53 @@ WIN_COMBINATIONS = [
   [1,4,7],
   [2,5,8],
   [0,4,8],
-  [6,4,2]
+  [2,4,6]
 ]
 
-
 def won?(board)
-  WIN_COMBINATIONS.find do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[0]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
+  WIN_COMBINATIONS.each do | win_combination |
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    all_O = [position_1, position_2, position_3].all? do | element |
+      element == "O"
+    end
+    all_X = [position_1, position_2, position_3].all? do | element |
+          element == "X"
+    end
+    if all_O || all_X
+      return win_combination
+    end
+
+    #if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1== "O" && position_2 == "O" && position_3 == "O")
+    #  return win_combination
+    # end
   end
+  # return false if no winning combination found
+  return false
 end
 
 def full?(board)
-  board.all? do |position|
-  position.include?("X") ||
-  position.include?("O")
- end
+  return board.all? {| element | element == "X" || element == "O"}
 end
 
 def draw?(board)
-  if won?(board)
-    false
-  else full?(board) && !won?(board)
-    true
+  if (!won?(board)) && full?(board)
+    return true
+  elsif (!won?(board) && !full?(board)) || won?(board)
+    return false
   end
 end
 
 def over?(board)
-  if full?(board)
-    true
-  elsif draw?(board)
-    true
-  elsif won?(board) && !full?(board)
-    true
-  end
+  (won?(board) || draw?(board))? true : false
+end
+
+def winner(board)
+  won?(board)? board[(won?(board))[0]] : nil
 end
