52af4cc694dcf71ce830ec683ada56edaa7477c5
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-03-13 00:17:53 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,6 +21,20 @@ def won?(board)
     return false
   elsif board[0] == "X" && board[1] == "X" && board[2] == "X"
     return WIN_COMBINATIONS[0]
+  elsif board[3] == "X" && board[4] == "X" && board[5] == "X"
+    return WIN_COMBINATIONS[1]
+  elsif board[6] == "X" && board[7] == "X" && board[8] == "X"
+    return WIN_COMBINATIONS[2]
+  elsif board[0] == "O" && board[3] == "O" && board[6] == "O"
+    return WIN_COMBINATIONS[3]
+  elsif board[1] == "O" && board[4] == "O" && board[7] == "O"
+    return WIN_COMBINATIONS[4]
+  elsif board[2] == "O" && board[5] == "O" && board[8] == "O"
+    return WIN_COMBINATIONS[5]
+  elsif board[0] == "X" && board[4] == "X" && board[8] == "X"
+    return WIN_COMBINATIONS[6]
+  elsif board[2] == "O" && board[4] == "O" && board[6] == "O"
+    return WIN_COMBINATIONS[7]
   end
 end
 
