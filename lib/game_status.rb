6480e87e1b114d46ce946a52dc51db1b99507d60
Joseph Parker <jparker010190@gmail.com>
2018-03-16 03:29:33 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,16 +8,14 @@ WIN_COMBINATIONS = [
   [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]
   ]
 def won?(board)
-winner = []
-empty_board = board.all? {|x| x == " "}
-WIN_COMBINATIONS.each do |sub_array|
-    if empty_board || full?(board)
-      return false
-    elsif sub_array.all? { |value| board[value] =="X" } || sub_array.all? { |value| board[value] =="O" }
-      winner = sub_array
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+      return win_combo
     end
+      false
   end
-  winner
 end
 
 def full?(board)
@@ -25,16 +23,19 @@ def full?(board)
 end
 
 def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  elsif !won?(board) && !full?(board) || !won?(board)
-    return false
-  end
+  !(won?(board)) && (full?(board))
 end
 def over?(board)
-  if !won?(board) && full?(board) && !draw(board) || won?(board) || draw(board)
+  if full?(board) || won?(board) || draw?(board)
     return true
   else
     return false
+  end 
+end  
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  else
+    return nil
   end
 end
