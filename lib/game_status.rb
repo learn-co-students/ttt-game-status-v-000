0df6039b5c232bae98b4afb4212da34e063e3be8
Joseph Parker <jparker010190@gmail.com>
2018-03-17 02:09:53 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,10 +3,20 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+board = [" "," "," "," "," "," "," "," "," "]
+
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
   ]
+#won?
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combo|
     if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
@@ -18,24 +28,31 @@ def won?(board)
   end
 end
 
+#full?
 def full?(board)
-  !board.any? { |x| x == " " }
+  board.all?{|occupied| occupied != " "}
 end
 
+#draw?
 def draw?(board)
   !(won?(board)) && (full?(board))
 end
+
+#over?
 def over?(board)
-  if full?(board) || won?(board) || draw?(board)
-    return true
-  else
-    return false
-  end 
-end  
+  (won?(board)) || (full?(board)) || (draw?(board))
+end
+
+#winner?
 def winner(board)
-  if winning_combo = won?(board)
-    board[winning_combo.first]
-  else
-    return nil
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
+      return "X"
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+      return "O"
+    else
+      nil
+    end
   end
 end
+# passed 5/6
