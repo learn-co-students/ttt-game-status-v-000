7435adc51fb0868299730fa529d5b0a0bcea73f7
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-03 07:32:26 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,24 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7][2,5,8][0,4,8][6,4,2]]
+
+def full?(board)
+  board.none?{|move| move == " "}
+end
+
+def won?(board)
+
+  for i in 0...WIN_COMBINATIONS.length
+    win = WIN_COMBINATIONS[i]
+
+    if win.all?{|idx| board[idx] == "O"}
+      return win
+    end
+    if win.all?{|idx| board[idx] == "X"}
+      return win
+    end
+
+  end
+  return false
+end
