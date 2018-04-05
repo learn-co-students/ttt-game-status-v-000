304dcd53e9a7f667e73b1f7429376aa78a4ee89e
Ji Yim <gsooyim@gmail.com>
2018-04-05 02:27:10 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,3 +1,4 @@
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
@@ -17,9 +18,7 @@ WIN_COMBINATIONS = [
   
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
-      return win_combination
-    elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+    if position_taken?(board, win_combination[2]) && board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] ==  board[win_combination[2]]
       return win_combination
     end
   end
