cd3224bbb9878cebf01de68fabc5fe9f5bdcf525
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-07 00:57:10 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,43 +1,3 @@
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-def won?(board)
-  WIN_COMBINATIONS.find {|win_combination|
-  board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]] && position_taken?(board, win_combination[0])
-}
-end
-
-def full?(board)
-if board.include?(' ') || board.include?('')
-  return false
-else
-    return true
-  end
-end
-
-def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  else !won?(board) && !full?(board)
-    return false
-  end
-end
-
-def over?(board)
-  if won?(board) || draw?(board) || full?(board)
-    return true
-    end
-end
-
-def winner(board)
-  if won?(board) != nil
-    winner = board[won?(board)[0]]
-end
-end
-
-# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -48,3 +8,83 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !(position_taken?(board, index))
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if !valid_move?(board, index)
+  turn(board)
+end
+      move(board, index, current_player(board))
+      display_board(board)
+end
+
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def turn_count(board)
+  # board.count{|token| token == "X" || token == "O"}
+
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
+    end
+  end
+  turns
+
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[0]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+board.all?{|token| token == "X" || token == "O"}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  if win = won?(board)
+    board[win.first]
+  end
+end
