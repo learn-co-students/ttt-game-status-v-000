300fcd997fad73fe71bb8a183ac5b665880eb952
Suchitra Naik <susanaik@gmail.com>
2018-04-21 21:35:28 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,81 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5], 
+  [6,7,8], 
+  [0,3,6], 
+  [1,4,7], 
+  [2,5,8], 
+  [0,4,8], 
+  [6,4,2]
+  ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination| 
+    
+     win_index_1  = win_combination[0] 
+     win_index_2  = win_combination[1] 
+     win_index_3  = win_combination[2] 
+
+     position_1 = board[win_index_1] 
+     position_2 = board[win_index_2] 
+     position_3 = board[win_index_3]
+     
+     
+      if(position_1=="X" && position_2=="X" && position_3=="X")
+          return win_combination 
+        elsif(position_1=="O" && position_2=="O" && position_3=="O")
+          return win_combination 
+      end
+  end
+  
+  return false
+  
+end
+
+def full?(board)
+	index=8
+	while(index>0)
+    if(position_taken?(board, index))
+	  	index-=1
+	  else
+	  	return false
+  	end
+  end
+  return true
+end
+
+def draw?(board)
+  if(!won?(board) && full?(board))
+      return true
+    else 
+      return false
+    end
+end
+
+
+def over?(board)
+  if(won?(board) || draw?(board) || full?(board))
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  
+  if(won?(board))
+       win_arr = won?(board)
+       if(board[win_arr[0]]=="X")
+            return "X"
+          else
+            return "O"
+      end
+    else
+      return nil
+    end
+  
+end
+
