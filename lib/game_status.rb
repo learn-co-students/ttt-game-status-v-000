0d4eedf519aeb744a727c6bcbf8014de84f0870b
Leticia Molina <letimolina@gmail.com>
2018-04-26 23:25:13 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,11 +9,11 @@ def empty_board?(board)
  end
 end
 
-def full_board?(board)
-  board.all? do |position|
-    position != nil || position != "" || position != " "
-  end
-end
+# def full_board?(board)
+#   board.all? do |position|
+#     position != nil || position != "" || position != " "
+#   end
+# end
 
 def draw?(board)
   if full_board?(board) == true
@@ -39,30 +39,6 @@ end
   [1,4,7], # Middle column
   [2,5,8] # Right column
 ]
- 
-# for each win_combination in WIN_COMBINATIONS
-  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-  # grab each index from the win_combination that composes a win.
-  # win_index_1 = WIN_COMBINATIONS[0]
-  # win_index_2 = win_combinations[1]
-  # win_index_3 = win_combinations[2]
-  # win_index_4 = win_combinations[3]
-  # win_index_5 = win_combinations[4]
-  # win_index_6 = win_combinations[5]
-  # win_index_7 = win_combinations[6]
-  # win_index_8 = win_combinations[7]
-  # win_index_9 = win_combinations[8]
-  
-
-  # position_1 = board[win_index_1] # load the value of the board at win_index_1
-  # position_2 = board[win_index_2] # load the value of the board at win_index_2
-  # position_3 = board[win_index_3] # load the value of the board at win_index_3
-  # position_4 = board[win_index_4] # load the value of the board at win_index_4
-  # position_5 = board[win_index_5] # load the value of the board at win_index_5
-  # position_6 = board[win_index_6] # load the value of the board at win_index_6
-  # position_7 = board[win_index_7] # load the value of the board at win_index_7
-  # position_8 = board[win_index_8] # load the value of the board at win_index_8
-  # position_9 = board[win_index_9] # load the value of the board at win_index_9
 
 def won?(board)
 WIN_COMBINATIONS.detect do |position| 
@@ -76,7 +52,6 @@ WIN_COMBINATIONS.detect do |position|
   end 
 end
 
-
 def full?(board)
   board.all?{|occupied| occupied != " "}
 end
@@ -85,7 +60,15 @@ def draw?(board)
   if !won?(board) && full_board?(board)
     true
   # elsif !won?(board) && !full_board?(board)
+  end
 end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    true
+  elsif !full?(board)
+    false
+  end
 end
   
 
