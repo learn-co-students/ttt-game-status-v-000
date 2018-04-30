10184414fb3b3cc67c405fff38380d13fe5e7a9e
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-04-30 17:30:59 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,51 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], #top row
+  [3, 4, 5], #middle row
+  [6, 7, 8], #bottom row
+  [0, 3, 6], #left column
+  [1, 4, 7], #middle column
+  [2, 5, 8], #right column
+  [0, 4, 8], #diagonal L to R
+  [2, 4, 6]  #diagonal R to L
+]  
+
+def won?(board)
+  WIN_COMBINATIONS.detect do | win_combination |
+    board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]] && position_taken?(board, win_combination[0])
+  end
+end
+
+def full?(board)
+  if board.include?('') || board.include?(' ')
+    return false
+  end
+  def draw(board) #did I solve this the wrong way, by nesting a method?
+    if !won?(board) && full?(board)
+     return true
+   end
+  end
+end
+    
+def draw?(board) #is this redundant code somehow?
+  if !won?(board) && full?(board)
+    return true
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+  return true
+  end
+end
+
+def winner(board)
+  winner = won?(board)
+  if board(winner[0]) == "X" && board(winner[1]) == "X" || board(winner[0]) == "X" && board(winner[2]) == "X"
+    return winner
+  else
+    return nil
+  end
+end
