2af65cace355fcc19b7ba6062f67471e529bf602
Catherine Bronzo <cbronzo@mac.com>
2018-06-03 19:40:43 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,41 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],  # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],  # Bottom row
+  [0,3,6],  # Left column
+  [1,4,7],  # Middle column
+  [2,5,8],  # Right column
+  [2,4,6],  # Diagonal 
+  [0,4,8],  # Diagonal
+]
+
+def won?(board)
+    WIN_COMBINATIONS.each do |combo|
+      if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
+      return combo
+      elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+      return combo
+      end
+    end
+      false
+  end  
+   
+def full?(board)
+ if board.include?(" ")
+  false
+  else 
+   true
+  end
+end  
+
+
+def draw?(board)
+end
+
+def over?(board)
+end
+
+def winner(board)
