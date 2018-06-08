5890f0ebd3d8d0f4f39a0739b24d11da8fa9e2a4
Niusha H <niusha.hadziomerovic@gmail.com>
2018-06-08 22:08:35 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,38 +15,66 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def won?(position_taken?)
-  # if empty board, return FALSE
-  if position_taken? == true 
-    return false 
-  # if draw, return FALSE
-  elsif position_taken?.full? == true && !WIN_COMBINATIONS
-    return false 
-  # if not empty board and not draw, then evaluate for winning combo 
-  else 
-    
-  end 
-end   
-
-
-
-position_taken?.detect do |winning_array|
-    if artist_element.class != Array
-      puts "Artist: #{artist_element}"
-    else
-      artist_element.each do |album_element|
-        if album_element.class != Array
-          puts "Album: #{album_element}"
-        else
-          album_element.each do |song_element|
-            puts "Song: #{song_element}"
-
-
 def won?(board)
-  if position_taken?.each do |winning_array|
-    winning_array.WIN_COMBINATIONS?
-  end
-end
+  
+  win_index_1 = WIN_COMBINATIONS[0]
+  win_index_2 = WIN_COMBINATIONS[1]
+  win_index_3 = WIN_COMBINATIONS[2]
+  
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+
+    # if empty board, return FALSE
+    if board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      return false 
+    # if not empty board and not draw, then evaluate for winning combo 
+    elsif
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination # return the win_combination indexes that won.
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination # return the win_combination indexes that won.
+      else
+        return false
+      end
+    else
+      return false
+    end
+end 
+
+#this passed!!!
+        # if draw, return FALSE
+    # elsif !WIN_COMBINATIONS == true
+    #   return false 
+
+
+#old thought on how to pass
+      # if board.each do |played_move|
+      #   played_move << check if position_taken?(board, 0..8)
+      #   end
+      #   WIN_COMBINATIONS
+      # else false
+      #   return "Next player's turn"
+  
+  
+# position_taken?.detect do |winning_array|
+#    if artist_element.class != Array
+#       puts "Artist: #{artist_element}"
+#     else
+#       artist_element.each do |album_element|
+#         if album_element.class != Array
+#           puts "Album: #{album_element}"
+#         else
+#           album_element.each do |song_element|
+#             puts "Song: #{song_element}"
+#     end
+
+# def won?(board)
+#   if position_taken?.each do |winning_array|
+#     winning_array.WIN_COMBINATIONS?
+#   end
+# end
+
 
 # Define full? Method
 
