546da95ea12998b18fbd0d6cf6f5364cbab3c252
Igli Cekani <htnacekan@gmail.com>
2018-06-08 06:50:16 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,31 +31,42 @@ WIN_COMBINATIONS =
 def won?(board)
 # what variable represents the board, and where does it come from?
 
-=begin WIN_COMBINATIONS.reject do |u|
-if position_taken?(board,u)
-binding.pry
-end
+  board.reject do |refuse_array|
 
-WIN_COMBINATIONS.detect do |element_array|
+    binding.pry
+     board[refuse_array[0]] == "X" || board [refuse_array[0]]== "O"
+     board[refuse_array[1]] == "X" || board [refuse_array[1]]== "O"
+     board[refuse_array[2]] == "X" || board [refuse_array[2]]== "O"
+     board[refuse_array[3]] == "X" || board [refuse_array[3]]== "O"
+     board[refuse_array[4]] == "X" || board [refuse_array[4]]== "O"
+     board[refuse_array[5]] == "X" || board [refuse_array[5]]== "O"
+     board[refuse_array[6]] == "X" || board [refuse_array[6]]== "O"
+     board[refuse_array[7]] == "X" || board [refuse_array[7]]== "O"
+     board[refuse_array[8]] == "X" || board [refuse_array[8]]== "O"
 #binding.pry
-  position_taken?(board, element_array)==true
-end
-=end
-  WIN_COMBINATIONS.reject do |refuse_array|
-     !position_taken?(board, refuse_array) && board[refuse_array] != " " && board[refuse_array] != nil
    end
-binding.pry
+return board[refuse_array]
+
+
+
 
   WIN_COMBINATIONS.select do |win_combination|
     # what is the condition that we want to match to return the win_combination array ?
     # 1. First postition must be takengh
     # 2. the second position must be the
     # 3. same as the first the same for the third
-  position_taken?(board, win_combination) && win_combination[0] == win_combination[1] && win_combination[1] == win_combination[2]
+  position_taken?(board, win_combination[0]) && win_combination[0] == win_combination[1] && win_combination[1] == win_combination[2]
+
   #position_taken?(board, win_combination[3]) && win_combination[3] == win_combination[4] && win_combination[4] == win_combination[5]
 #binding.pry
   end
 
+
+
+  #position_taken?(board, win_combination[3]) && win_combination[3] == win_combination[4] && win_combination[4] == win_combination[5]
+
+
+return win_combination
 
 
 #binding.pry
