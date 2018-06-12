deb221b372ed1788d92b41dab36d8534b18a29d6
Igli Cekani <htnacekan@gmail.com>
2018-06-12 02:03:42 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,12 +42,23 @@ index_0 = win_combination[0]
 index_1 = win_combination[1]
 index_2 = win_combination[2]
 
+
  position_1= board[index_0]
  position_2= board[index_1]
  position_3= board[index_2]
 
 position_taken?(board, win_combination[0]) && position_1 == position_2 && position_2 == position_3
 
+
+# what variable represents the board, and where does it come from?
+  binding.pry
+        WIN_COMBINATIONS.detect do |win_combination|
+
+          # what is the condition that we want to match to return the win_combination array ?
+          # 1. First postition must be taken
+          # 2. the second position must be the
+          # 3. same as the first the same for the third
+          position_taken?(board, win_combination[0]) && win_combination[0] == win_combination[1] && win_combination[1] == win_combination[2]
 
 end
 end
