92b17df8418fcb7f9dfc1fca15a4ebc0d0774f87
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-18 22:18:24 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,8 +3,6 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
-
 WIN_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -21,24 +19,20 @@ def won?(board)
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
-    win_index_4 = win_combination[3]
-    win_index_5 = win_combination[4]
-    win_index_6 = win_combination[5]
-    win_index_7 = win_combination[6]
-    win_index_8 = win_combination[7]
-  
-    position_1 = board[win_index_1] 
+
+    position_1 = board[win_index_1]
     position_2 = board[win_index_2]
-    position_3 = board[win_index_3] 
-   
-    if position_1 == "X" && position_2 == "X" && position_3 == "X" ||
-       position_1 == "O" && position_2 == "O" && position_3 == "O"
-      
-     return win_combination
-    else 
-      return false
-    end
+    position_3 = board[win_index_3]
+  
+  if
+    position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+    position_1 == "O" && position_2 == "O" && position_3 == "O"
+  
+    return win_combination
   end
+ end
+ else 
+   false
 end
 
 def full?(board)
@@ -51,4 +45,7 @@ def draw?(board)
   !won?(board) && full?(board)
 end
 
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
   
