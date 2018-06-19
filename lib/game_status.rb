51d0bdabe7808c4c17013b4777ebcb657131f443
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-19 16:57:31 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -72,6 +72,7 @@ def winner(board)
     if 
       position_1 == "X" && position_2 == "X" && position_3 == "X" 
       return "X"
+    
     elsif 
        position_1 == "O" && position_2 == "O" && position_3 == "O"
       return "O"
