91ee47412b02e65bd4de726d4abe15d028cac01a
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-19 16:49:12 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -59,7 +59,26 @@ else
 end
 
 def winner(board)
-  winning_token = won?(board)
+  if won?(board)
+   WIN_COMBINATIONS.each do |win_combination| 
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if 
+      position_1 == "X" && position_2 == "X" && position_3 == "X" 
+      return "X"
+    elsif 
+       position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return "O"
+        end
+      end
+    end
+end
   
 
 
