57cc753b28c35d080be07b88ec75702804affc9d
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-19 16:40:09 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination| 
+    WIN_COMBINATIONS.each do |win_combination| 
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -25,9 +25,8 @@ def won?(board)
     position_3 = board[win_index_3]
     
     if 
-      position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+      position_1 == "X" && position_2 == "X" && position_3 == "X" || 
       position_1 == "O" && position_2 == "O" && position_3 == "O"
-      
       return win_combination
     else
       false
@@ -56,12 +55,19 @@ def over?(board)
   won?(board) || draw?(board) || full?(board)
   end
 else 
-  false
+  return false
 end
 
 def winner(board)
-  win_combination
+  winning_token = won?(board)
   
+
+
+
+
+
+    
+
     
 
   
