954c8439d57dd75c52dcbc9e3e3500b3f236f059
Hayden Betts <haydenbetts@gmail.com>
2018-06-21 22:55:37 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,39 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+]
+
+def won?(board)
+  # confirm that there are three of the same token at the indices defined in win combinations
+  WIN_COMBINATIONS.detect do |combination|
+    position_1 = board[combination[0]]
+    position_2 = board[combination[1]]
+    position_3 = board[combination[2]]
+
+    # TODO look at on 6/21 and consider to refactoring
+    ([position_1, position_2, position_3].all? { |elt| elt == "X"} ||
+    [position_1, position_2, position_3].all? { |elt| elt == "O"})
+
+  end
+end
+
+def full?(board)
+end
+
+def draw?(board)
+end
+
+def over?(board)
+end
+
+def winner?(board)
+end
