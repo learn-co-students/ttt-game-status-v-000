ce3ae28574ec2e9bef4019ab6dee53df656572d8
tc.thomascho@gmail.com <tc.thomascho@gmail.com>
2018-06-22 13:17:10 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,21 +8,21 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
 
 def won?(board)
   WIN_COMBINATIONS.detect do |winning_combination|
-    board[winning_combination[0]] == "X" && board[winning_combination[1]] == "X" && board[winning_combination[2]] == "X" || board[winning_combination[0]] == "O" && board[winning_combination[1]] == "O" && board[winning_combination[2]] == "O"
+    board[winning_combination[0]] == board[winning_combination[1]] && board[winning_combination[1]] == board[winning_combination[2]] && board[winning_combination[2]] == "X" || board[winning_combination[0]] == board[winning_combination[1]] && board[winning_combination[1]] == board[winning_combination[2]] && board[winning_combination[2]] == "O"
   end
 end
 
 
 def full?(board)
-  board.none? {|el| el == " " || el == "" || el== nil}
+  board.all? {|el| el == "X" || el == "O"}
 end
 
 def draw?(board)
-  !(won?(board)) && full?(board) ? true : false
+  !(won?(board)) && full?(board)
 end
 
 def over?(board)
-  won?(board) || full?(board) || draw?(board) ? true : false
+  won?(board) || full?(board) || draw?(board)
 end
 
 def winner(board)
