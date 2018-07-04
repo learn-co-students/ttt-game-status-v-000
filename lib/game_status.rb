71c8cc546ef687f7d8c2b5315d901b2f67d4f21e
Anna Dybas <a.m.dybas@gmail.com>
2018-07-04 22:29:34 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,12 +5,20 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5], # Middle row
+  [0,1,2], # Top row [0]
+  [3,4,5], # Middle row [1]
   [6,7,8], # Bottom row
   [0,3,6], # First column
   [1,4,7], # Second column
   [2,5,8], # Third column
   [0,4,8], # Top Left diagonal
-  [2,4,6] # Top Right diagonal
+  [2,4,6] # Top Right diagonal [8]
 ]
+
+def won?(board)
+  blah = []
+  board.each do ||
+
+  end
+  blah
+end
