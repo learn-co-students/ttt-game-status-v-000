ffdc1e0f1b4cd0b8b47553b8ad3e985c9cb60ee1
Jonathan Fernandez <jfern20@gmail.com>
2018-07-05 15:05:29 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,14 +15,15 @@ WIN_COMBINATIONS = [
   [1,4,7], #Middle column
   [2,5,8], #Right column
   [0,4,8], #Left start diagonal
-  [2,4,6]  #Right start diagonal
+  [6,4,2]  #Right start diagonal
 ]
 
-def  won?(board)
-  WIN_COMBINATIONS.each {|win_combo|
+ 
+  def won?(board)
+  WIN_COMBINATIONS.each |win_combo|
   
- win_index_0 = win_combo[0]
-  win_index_1 = win_combo[1]
+   win_index_0 = win_combo[0]
+   win_index_1 = win_combo[1]
    win_index_2 = win_combo[2]
    
    position_1 = board[win_index_0]
@@ -30,5 +31,12 @@ def  won?(board)
    position_3 = board[win_index_2]
   
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return WIN_COMBINATIONS
-end
+    return win_combo
+    
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return win_combo
+  end
+  
+    return false
+ 
+ end
