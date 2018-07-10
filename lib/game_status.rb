188a44909ca48a2da4beb680f4faa292c3fc2681
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-10 14:10:25 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,20 @@
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
+WIN_COMBINATIONS = [
+  [0, 1, 2], #Top Row
+  [3, 4, 5], #Middle Row
+  [6, 7, 8], #Bottom Row
+  [0, 3, 6], #Left Column
+  [1, 4, 7], #Middle Column
+  [2, 5, 8], #Right Column
+  [0, 4, 8], #Left Diagonal
+  [2, 4, 6] #Right Diagonal
+]
 
-# Define your WIN_COMBINATIONS constant
+def won?(board) WIN_COMBINATIONS.each do |wincombo|
+  if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X"
+    return wincombo
+  elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O"
+    return wincombo
+  end
+end
+false
+end
