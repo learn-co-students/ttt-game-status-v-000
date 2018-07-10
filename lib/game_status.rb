50d31accecdd959987a547100b51bd84974ef007
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-10 17:39:27 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,12 +9,39 @@ WIN_COMBINATIONS = [
   [2, 4, 6] #Right Diagonal
 ]
 
-def won?(board) WIN_COMBINATIONS.each do |wincombo|
-  if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X"
+def won?(board)
+  WIN_COMBINATIONS.each do |wincombo|
+  if board[wincombo[0]] == "X" && board[wincombo[1]] == "X" && board[wincombo[2]] == "X"
     return wincombo
-  elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O"
+  elsif board[wincombo[0]] == "O" && board[wincombo[1]] == "O" && board[wincombo[2]] == "O"
     return wincombo
   end
 end
 false
+end
+
+def full?(board)
+  board.all? {|i| i == "X" || i == "O"}
+end
+
+def draw?(board)
+    if !won?(board) && full?(board)
+      return true
+    else
+      return false
+    end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
 end
