486dbfb4fb62c3bc32ef798153b25dde1a942cdc
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-14 20:23:55 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,27 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], # Top row
+  [3, 4, 5], # Middle row
+  [6, 7, 8], # Bottom row
+  [0, 3, 6], # Left column
+  [1, 4, 7], # Middle column
+  [2, 5, 8], # Right column
+  [0, 4, 8], # Diagonal
+  [2, 4, 6]  # Diagonal
+]
+
+# Check to see if there was a win
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_combination.each do |win_index|
+      # index = win_index
+      if position_taken?(board, index) == true
+        
+      else
+        return false
+      end
+    end
+  end
+end
