fcbc52503500c4274f4b3d6842e0b89fbc27415a
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-14 22:46:06 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,19 +11,61 @@ WIN_COMBINATIONS = [
   [0, 3, 6], # Left column
   [1, 4, 7], # Middle column
   [2, 5, 8], # Right column
-  [0, 4, 8], # Diagonal
-  [2, 4, 6]  # Diagonal
+  [0, 4, 8], # Left Diagonal
+  [2, 4, 6]  # Right Diagonal
 ]
 
 # Check to see if there was a win
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    win_combination.each do |win_index|
-      # index = win_index
-      if position_taken?(board, index) == true
-        
-      else
-        return false
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+    end
+  end
+  return false
+end
+
+# Check to see if the board is full
+def full?(board)
+  board.all? {|position| position == "X" || position == "O"}
+end
+
+# Check to see if the game is a draw
+def draw?(board)
+  full?(board) && !(won?(board))
+end
+
+# Check to see if the game is over
+def over?(board)
+  draw?(board) || won?(board)
+end
+
+# Determine who is the winner
+def winner(board)
+  if won?(board)
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return "X"
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return "O"
       end
     end
   end
