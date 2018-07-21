b581ee2b4eb5ee896f87946609ec084b3204b0d7
NaKia Whitby <whitby.nakia@gmail.com>
2018-07-21 21:23:25 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,8 +6,8 @@ end
 # Define your WIN_COMBINATIONS constant
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],  # Middle row
+  [0,1,2],
+  [3,4,5],  
   [6,7,8],
   [0,3,6],
   [1,4,7],
