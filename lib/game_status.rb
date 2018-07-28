a0876dc94676442767fe8c80c938654814bb2709
Melissa Hummelt <mkhummelt@gmail.com>
2018-07-28 21:54:49 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,28 +17,44 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
+    first_position  = combo[0]
+    second_position = combo[1]
+    third_position  = combo[2]
+
+    first = board[first_position]
+    second = board[second_position]
+    third = board[third_position]
+
+    if first == "X" && second == "X" && third == "X"
+      true
+    elsif first == "O" && second == "O" && third == "O"
+      true
+    else
+      false
+    end
   end
 end
 
 def full?(board)
-  board.all? {|space|
-    space == "X" || space == "O"}
+  board.all? do |space|
+    if (space == "X" || space == "O")
+      true
+    else
+      false
+    end
+  end
 end
 
 def draw?(board)
-  full?(board)
-  if won?(board)
-    false
-  else
+  if full?(board) && !won?(board)
     true
+  else
+    false
   end
 end
 
 def over?(board)
-  if won?(board) || full?(board)
+  if won?(board) || full?(board) || draw?(board)
     true
   else
     false
@@ -46,7 +62,12 @@ def over?(board)
 end
 
 def winner(board)
-  if combo = won?(board)
-    board[combo.first]
+  winning_combo = won?(board)
+  if winning_combo.class != Array
+    nil
+  else
+    winning_index = winning_combo[0]
+    winning_token = board[winning_index]
   end
+
 end
