f0d6864a5cc1ea2356abc42d0899d9360f3b8b0b
Austin Powell <austinpowell58@gmail.com>
2018-08-02 01:15:07 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,68 @@
+
+
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+  
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    if win_combo.all?{|win_index| board[win_index] == "X"}
+      return win_combo
+    elsif win_combo.all? {|win_index| board[win_index] == "O"}
+      return win_combo
+    end 
+  end
+  return false
+end 
+      
+      
+def full?(board)
+  board.all? do |index|
+    index == "X" || index == "O"
+  end
+end 
+
+def draw?(board)
+  if full?(board) && won?(board) == false
+    TRUE
+  end 
+end 
+
+def over?(board)
+  if won?(board) != false || draw?(board)
+    TRUE
+  end
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    if win_combo.all?{|win_index| board[win_index] == "X"}
+      return "X"
+    elsif win_combo.all? {|win_index| board[win_index] == "O"}
+      return "O"
+    end 
+  end
+  return nil
+end 
+    
+      
+  
+
+  
+
+
+  
