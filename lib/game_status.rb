bb0ccb52776c50021b910f0ed36b3d8d824ce13a
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-08-11 19:02:29 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,42 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [0,3,6],
+  [1,4,7],
+  [2,4,6],
+  [2,5,8],
+  ]
+  
+  def won?(board)
+    
+    WIN_COMBINATIONS.detect do |combos|
+      board[combos[0]] == board[combos[1]] && board[combos[2]] == board[combos[1]] && position_taken?(board, combos[0])
+    end
+  end
+  
+  def full?(board)
+    board.all? {|token| token == "X" || token == "O"}
+  end
+  
+  def draw?(board)
+    full?(board) && !won?(board)
+  end
+  
+  def over?(board)
+    draw?(board) or won?(board) or full?(board)
+  end
+  
+  def winner(board)
+    winner = won?(board)
+    
+    if winner 
+      index = winner[0]
+      board[index]
+    end
+    
+  end
