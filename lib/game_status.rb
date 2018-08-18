c20ad135823348c70d94b64b3b0fbd2f89bda40c
Ashley Wong <ashwong04@gmail.com>
2018-08-18 15:25:12 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 
-WIN_COMBINATIONS=[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+WIN_COMBINATIONS=[[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
 def won?(board)
   if board.none? do |empty|
@@ -18,9 +18,15 @@ def won?(board)
     win_index_3 = win_combination[2]
     
     position_1 = board[win_index_1]
-    position_1 = board[win_index_1]
-    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination 
+    else
+      false
+    end
+    
     end      
   end
 end
-
