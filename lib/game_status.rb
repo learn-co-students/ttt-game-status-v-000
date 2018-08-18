55bd71f39a5aea7d215daf758d81d9b281ee5e08
Ashley Wong <ashwong04@gmail.com>
2018-08-18 14:14:09 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,14 +10,21 @@ WIN_COMBINATIONS=[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 
 def won?(board)
   if board.none? do |empty|
       empty != " " || empty != ""
-   end   
-  end    
-      
-  
-  #elsif board.any? do |moves|
-   # WIN_COMBINATION
-
+      end
     
-      
+  #winning_array = []
+  elsif WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    win_index_4 = win_combination[3]
+    win_index_5 = win_combination[4]
+    win_index_2 = win_combination[6]
+    win_index_6 = win_combination[7]
+    win_index_7 = win_combination[8]
+    
+    
+    end      
+  end
 end
 
