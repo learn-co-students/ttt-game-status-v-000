ad0b0e2e3beec263cd3780d5b06735550501d2b8
Ashley Wong <ashwong04@gmail.com>
2018-08-18 15:45:45 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
     
-    if position_1 == "X" && position_2 == position_1 && position_3 == position_2
+    if position_1 == "X" && position_2 == position_1 && position_3 == position_1
       return win_combination 
     else
       false
