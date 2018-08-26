500515e1175cf7954dbd22fb26ff39a9db5f6498
Stef Claus <stef.claus@gmail.com>
2018-08-26 21:06:26 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -55,3 +55,22 @@ def over?(board)
     true
   end #ends if
 end #ends over 
+
+def winner(board)
+  if won(board)
+   WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2] 
+    position_3 = board[win_index_3] 
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return "X" 
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return "O"
+      end #other if  
+    end #end win_combinations
+  end #end if
+end #end winner
