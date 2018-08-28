7a56a816b4046d40c94a19ae0626af0d750766ea
Cheryl K <cherylkelson@icloud.com>
2018-08-28 15:12:38 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 # Helper Method
 def full?(board)
    board.all? {|b| b != " " }
@@ -23,6 +23,7 @@ end
 # end
 
 
+
 #end
 
 def draw?(board)
@@ -31,20 +32,6 @@ end
 
 def over?(board)
   draw?(board) || won?(board) 
-end
-
-def winner?(board)
-  if X == [0,1,2]|| [3,4,5] || [6,7,8] || [0,3,6] || [1,4,7] || [2,5,8] || [0,4,8] || [2,4,6] 
-    return winner
-  else
-     O = [0,1,2] || [34,5] || [6,7,8] || [0,3,6] || [1,4,7] || [2,5,8] || [0,4,8] || [2,4,6]
-      #return winner
-      
-    elseif X != && Y != 
-      #return ??
-      # I think I need to use .any somewhere in this method
-  
-  
 end
 
 def won?(board)
@@ -64,11 +51,20 @@ def won?(board)
    
     if position_1 == "X" && position_2 == "X" && position_3 == "X" 
       return win_combination 
-      elsif position_1 == "O" && position_2 == "O" && position_3 == "O" 
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O" 
       return win_combination
     else
       false
     end
   end
   false
+end
+
+def winner(board)
+  winning_array = won?(board) #Setting winning_array to equal results from won?(board)
+  if won?(board) # Implicit return of boolean -> if true return which character won
+    return board[winning_array[0]] #
+  else
+    return nil
+  end
 end
