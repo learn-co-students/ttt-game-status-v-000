3d85f1dfa4bc6248f7210532ed9ecef69b73382f
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-10 00:01:34 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -45,3 +45,28 @@ def winner(board)
   end 
 end
 
+#def won?(board)
+  #WIN_COMBINATIONS.detect do |combo|
+    #board[combo[0]] == board[combo[1]] &&
+    #board[combo[1]] == board[combo[2]] &&
+    #position_taken?(board, combo[0])
+  #end
+#end
+
+#def full?(board)
+  #board.all?{|token| token == "X" || token == "O"}
+#end
+
+#def draw?(board)
+  #full?(board) && !won?(board)
+#end
+
+#def over?(board)
+  #won?(board) || full?(board)
+#end
+
+#def winner(board)
+  #if winning_combo = won?(board)
+    #board[winning_combo.first]
+  #end
+#end
