c247bdeee23770bd00f41b4d05eb3506568751fb
Toni-Ty <antonique2479@yahoo.com>
2018-09-22 05:27:20 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,13 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8]
+  [0,3,6],
+  [1,4,7]
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
