d6df6592c2dc09e551f53b5ed71eb54fe234b9e7
Toni-Ty <antonique2479@yahoo.com>
2018-09-22 06:29:45 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,10 +6,19 @@ end
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
-  [6,7,8]
+  [6,7,8],
   [0,3,6],
-  [1,4,7]
+  [1,4,7],
   [2,5,8],
   [0,4,8],
   [2,4,6]
 ]
+def won?(board)
+  WIN_COMBINATIONS.select do |win_combination|
+  if win_combination == "X" && win_combination == "X" && win_combination == "X"
+    return win_combination
+  else
+    false
+    end
+  end
+end
