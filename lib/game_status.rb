d5030b209d08763b8e7ae484a6b7bc934f5b5e2c
Toni-Ty <antonique2479@yahoo.com>
2018-09-28 00:00:00 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,29 +26,55 @@ def won?(board)
 end
 #only had to tell the block what to do for the first Iterate, and then it knows to go through all of the array indexes
 
-def full? (board)
-  WIN_COMBINATIONS.all? do |full|
-  if board[full[0]] == "X" && board[full[1]] == "X" && board[full[2]] == "X" &&
-     board[full[0]] == "O" && board[full[1]] == "O" && board[full[2]] == "O"
-    return true
+def full?(board)
+  if board.all? {|space| space == "X"  || space == "O"}
+      return true
   else
-    return false
+      return false
     end
   end
-end
 
-def draw? (board)
-  if won?(board)
-    return false
-  else
-    return true
-  end
-end
+    #  board[full[0]] == "O" && board[full[1]] == "O" && board[full[2]] == "O"
 
+
+    def draw?(board)
+      if board.all? {|index| index == "X" || index == "O" && !won?(board)}
+          return true
+        else
+          return false
+        end
+      end
+# def draw?(board)
+#   board.all? do |index|
+#     if index == "X" || index == "O" || index == !won?
+#       return true
+#     else
+#       return false
+#     end
+#   end
+# end
 def over?(board)
-  if won?(board) ||
+  if won?(board) || draw?(board) || full?(board)
     return true
   else
     return false
   end
 end
+
+# def winner?(board)
+#   if !won?(board)
+#     return nil
+#   if board[won?(board)[0]] == "X"
+#     return "X"
+#   else
+#     return "Y"
+#     end
+#   end
+# end
+def winner(board)
+    if won?board
+      return board[won?(board)[0]]
+    else
+      return nil
+    end
+  end
