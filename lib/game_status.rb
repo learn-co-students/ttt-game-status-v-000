05fcec80d3babdc57fa017908710bc37c9e2081e
Angie Goza <angielg9888@gmail.com>
2018-10-08 16:31:00 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,8 +4,8 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [[0,1,2],
-                    [3,4,5],
+WIN_COMBINATIONS = [[0,1,2], # win_index 1
+                    [3,4,5], # win_index 2
                     [6,7,8],
                     [0,3,6],
                     [1,4,7],
@@ -16,24 +16,23 @@ WIN_COMBINATIONS = [[0,1,2],
   
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-  # grab each index from the win_combination that composes a win.
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-  
- 
-  position_1 = board[win_index_1] # load the value of the board at win_index_1
-  position_2 = board[win_index_2] # load the value of the board at win_index_2
-  position_3 = board[win_index_3] # load the value of the board at win_index_3
-  
- 
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination # return the win_combination indexes that won.
-  elseif position_1 == "O" && position_2 == "O" && position_3 == "O"
-    return win_combination
-  else
-    false
+      # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+      # grab each index from the win_combination that composes a win.
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      
+      
+      position_1 = board[win_index_1] # load the value of the board at win_index_1
+      position_2 = board[win_index_2] # load the value of the board at win_index_2
+      position_3 = board[win_index_3] # load the value of the board at win_index_3
+      
+        
+        if position_1 == "X" && position_2 == "X" && position_3 == "X"
+          return win_combination # return the win_combination indexes that won.
+        elseif position_1 == "O" && position_2 == "O" && position_3 == "O"
+          return win_combination
+        end
   end
-end
+  nil
 end
