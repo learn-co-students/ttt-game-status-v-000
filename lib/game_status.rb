fdcea6fc25a8f6ab7a420347a5b4185707e42c48
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-10-23 11:09:56 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,27 +8,27 @@ WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
   [6,7,8],
-  [0,4,8],
-  [2,4,6],
   [0,3,6],
+  [1,4,7],
   [2,5,8],
-  [1,4,7]
+  [0,4,8],
+  [2,4,6]
 ]
 
 def won?(board)
 WIN_COMBINATIONS.detect do |combo|
   board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
+  board[combo[0]] == board[combo[2]] &&
+  position_taken?(board, combo[0])
 end
 end
 
 def full?(board)
-board.all? do |piece|
-  piece == "X" || piece == "O"
+board.all? do |token|
+  token == "X" || token == "O"
 end
 end
 
 def draw?(board)
-  full?(board) && !won?(board)
+full?(board) && !won?(board)
 end
