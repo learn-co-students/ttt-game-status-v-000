8397d7d88d6d7be2b3deed8cbd69ee5635e982c1
d-d-d-dalia <sawaya.dalia@gmail.com>
2018-10-26 12:51:51 -0700

    update winner(board) because the solution is dum

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -44,7 +44,15 @@ def over?(board)
 end
 
 def winner(board)
-  if winning_combo = won?(board)
-  board[winning_combo[0]]
+  if won?(board)
+    return board[won?(board)[0]]
   end
 end
+# OR
+# def winner(board)
+#   if won?(board)
+#     win_combo = won?(board)
+#     return board[win_combo.last]
+#   end
+# end
+# ETC
