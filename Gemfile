53bd1622a97472519524b839bd4594381cf6ee5b
bkhartmire <bkhartmire@gmail.com>
2019-03-11 15:18:13 +0900

    Done.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@
 source "https://rubygems.org"
 
 # gem "rails"
-
+gem 'rb-readline'
 gem 'rspec'
     gem 'pry'
--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,18 +17,11 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
-    win_index_1 = combo[0]
-    win_index_2 = combo[1]
-    win_index_3 = combo[2]
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+    if (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X") || (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O")
       return combo
     end
   end
-else
-  false
+  return false
 end
 
 def full?(board)
