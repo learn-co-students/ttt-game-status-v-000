99472276accf5458edb11577ba904b549950c187
Olivia Kesler <oakesler@protonmail.ch>
2019-05-24 21:02:57 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,26 @@
-# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[2, 5, 8],[1, 4, 7],[0, 3, 6],[0, 4, 8],[6, 4, 2]]
+
+def won?(board)
+ 
+  def won?(board)
+WIN_COMBINATIONS.select do |combo|
+  board[combo[0]] && board[combo[1]] && board [combo[2]] == "X"
+  end
+end
+
+  
+def full?(board)
+end
+
+def draw?(board)
+end
+
+def over?(board)
+end
+
+def winner?(board)
+end
