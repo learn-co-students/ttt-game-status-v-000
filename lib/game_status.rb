8a637837bd4e3246f5c27654aecd2ab6e88c9189
Tom Sharkey <tbsharkey@gmail.com>
2019-08-13 23:46:54 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,14 +17,12 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-WIN_COMBINATIONS.detect do |combo|
-
-    position_1 = board[combo[0]] # load the value of the board at win_index_1
-    position_2 = board[combo[1]] # load the value of the board at win_index_2
-    position_3 = board[combo[2]] # load the value of the board at win_index_3
-
-    position_1 == position_2 && position_2 == position_3 && position_taken?(board, combo[0])
-end
+  WIN_COMBINATIONS.detect do |combo|
+      position_1 = board[combo[0]] # load the value of the board at win_index_1
+      position_2 = board[combo[1]] # load the value of the board at win_index_2
+      position_3 = board[combo[2]] # load the value of the board at win_index_3
+      position_1 == position_2 && position_2 == position_3 && position_taken?(board, combo[0])
+  end
 end
 
 def full?(board)
@@ -32,15 +30,13 @@ def full?(board)
 end
 
 def draw?(board)
-!won?(board) && full?(board)
+  !won?(board) && full?(board)
 end
 
 def over?(board)
-won?(board) || draw?(board)
+  won?(board) || draw?(board)
 end
 
 def winner(board)
-  if won?(board)
-      board[won?(board)[0]]
-  end
+  board[won?(board)[0]] if won?(board)
 end
