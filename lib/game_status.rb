b6d0cd0ec64944c0c7e3471c6e303935c6889a22
Dominique De León <dominique.a.deleon@gmail.com>
2019-09-10 18:29:58 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,68 +1,28 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
 # Define your WIN_COMBINATIONS constant
+
+
 WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5], # Middle row
-  [6,7,8], #bottom row
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #top row
+  [0,4,8], #diagonal
+  [2,4,6], #diagonal
   [0,3,6], #left column
   [1,4,7], #middle column
-  [2,5,8], #right column
-  [2,4,6], #first diagonal
-  [0,4,8] #second diagonal
-  # ETC, an array for each win combination
-]
+  [2,5,8] #right column
+ ]
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+  
+  WIN_COMBINATIONS.each do |combos|
 
-    position_1 = board[win_index_1] # load the value of the board at win_index_1
-    position_2 = board[win_index_2] # load the value of the board at win_index_2
-    position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-        # (board[0] == "X" && win_combination[1] == "X" && win_combination[2] == "X") ||
-        # (winning[0] == "O" && win_combination[1] == "O" && win_combination[2] == "O")
-        # if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
-        #   (position_1 == "O" && position_2 == "O" && position_3 == "O")
-        #   return win_combination # return the win_combination indexes that won.
-        # else
-        #   false
-        # end
-        #redundant!
-
-        #(position_1 == "X" && position_2 == "X" && position_3 == "X") ||
-        #  (position_1 == "O" && position_2 == "O" && position_3 == "O")
-
-        position_1 == position_2 && position_1 == position_3 && position_taken?(board, win_index_1)
-  end
-end
-
-def full?(board)
-  board.all? do |full|
-    full == "X" || full == "O"
-  end
-end
-
-def draw?(board)
-  full?(board) && !won?(board)
-end
-
-def over?(board)
-  draw?(board) || won?(board)
-end
-
-def winner(board)
-  if won?(board)
-    return board[won?(board)[0]]
-  else
-    nil
+    if combos[0] && combos[1] && combos[2] == "X"
+      return combos
+    elsif combos[0] && combos[1] && combos[2]  == "O"
+      return combos
+    else
+      return false
+    end
+    
   end
 end
