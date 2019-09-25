ec8a34f19197b960178d5ef271805fd9402bb71a
mstmari <michael.landrum.st.marie@gmail.com>
2019-09-25 14:35:51 -0400

    Restarted lab

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,60 +1,20 @@
 
-
+board = [" "," "," "," "," "," "," "," "," "]
 # Define your WIN_COMBINATIONS constant
 require 'pry'
 # Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
+WIN_COMBINATIONS = [[0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+                    [0,3,6],
+                    [1,4,7],
+                    [2,5,8],
+                    [0,4,8],
+                    [6,4,2]]
 
-
-# Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [6,4,2],
-  [0,4,8]
-]
-
-def won? (board)
-  WIN_COMBINATIONS.find do |w|
-
-    win_index_1 = w[0]
-    win_index_2 = w[1]
-    win_index_3 = w[2]
-    board[win_index_1] == board[win_index_2] && board[win_index_2] == board[win_index_3] &&  position_taken?(board, win_index_1)
-  end
-end
-
-def full?(board)
-  board.all? do |token|
-   token == "X" || token == "O"
-    end
-  end
-
-
-  def draw?(board)
-    full?(board) && !won?(board)
-  end
-
-  def over? (board)
-    if draw?(board)
-      true
-    elsif won?(board)
-      true
-    else
-      false
-      end
-end
-
-
-def winner (board)
-  if won?(board)
-    board[won?(board)[0]]
-    #binding.pry
-end
+def won?(board)
+  win_index_1 = WIN_COMBINATIONS[0]
+  win_index_2 = WIN_COMBINATIONS[1]
+  win_index_3 = WIN_COMBINATIONS[2]
+binding.pry
 end
