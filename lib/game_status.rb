a0834691ac59634a3ad0c78bd489a9e0a766b011
Dominique De León <dominique.a.deleon@gmail.com>
2020-01-23 00:20:40 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,5 +1,5 @@
 # Define your WIN_COMBINATIONS constant
-require "pry"
+
 
 WIN_COMBINATIONS = [
   [0,1,2], #top row
@@ -11,30 +11,18 @@ WIN_COMBINATIONS = [
   [1,4,7], #middle column
   [2,5,8] #right column
  ]
- 
-# method we found when googling around 
-# def same_values?  
-#   self.uniq.length == 1
-# end
 
 def won?(board)
-  i = 0
-  while i < WIN_COMBINATIONS.length
-    if WIN_COMBINATIONS[i][3] && WIN_COMBINATIONS[i][4] && WIN_COMBINATIONS[i][5] == "X"
+
+  WIN_COMBINATIONS.each do |combos|
+
+    if combos[0] && combos[1] && combos[2] == "X"
+      return combos
+    elsif combos[0] && combos[1] && combos[2]  == "O"
+      return combos
+    else
+      return false
     end
-    i += 1
+
   end
-  
-  # WIN_COMBINATIONS.each do |combos|
-
-  #   if combos[3] && combos[4] && combos[5] == "X"
-  #     return nil
-  #   elsif combos[0] && combos[1] && combos[2]  == "O"
-  #     return combos
-  #   else
-  #     return false
-  #   end
-
-  # end
- 
 end
