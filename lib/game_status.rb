10cb704950819ef85ddc466482863f04cdb618d2
David Schoenholtz <dmschoenholtz@gmail.com>
2020-04-21 19:15:21 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,59 +5,38 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # Diagonal top left to bottom right
+  [2,4,6], # Diagonal top right to bottom left
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-    position_1 = board[win_index_1] # load the value of the board at win_index_1
-    position_2 = board[win_index_2] # load the value of the board at win_index_2
-    position_3 = board[win_index_3] # load the value of the board at win_index_3
-    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
-    return win_combination # return the win_combination indexes that won.
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
-end
-  else
-    false
 end
 
 def full?(board)
-  board.all? do |space|
-    space == "X" || space == "O"
-  end
+  board.all? {|position| position == "X" || position == "O"}
 end
 
 def draw?(board)
-  if full?(board) && !won?(board)
-  return true
-else
-  false
-end
+  !won?(board) && full?(board)
 end
 
 def over?(board)
-  if won?(board) || draw?(board) || full?(board)
-    return true
-  end
+  won?(board) || full?(board) || draw?(board)
 end
 
 def winner(board)
-  winning_token = won?(board)
- if winning_token == false
- return nil
- elsif board[winning_token[0]] == "X"
-   return "X"
- elsif board[winning_token[0]] == "O"
-   return "O"
-end
+  if token = won?(board)
+    board[token[0]]
+  end
 end
