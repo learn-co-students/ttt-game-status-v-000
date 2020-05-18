092d911e944e2f8d8209793b9f84476d5e7c24b8
Hieu Le <hieuhuule@gmail.com>
2020-05-18 07:19:24 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,6 +3,19 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+# Define won?, full?, draw?, over?, and winner below
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
@@ -28,14 +41,3 @@ def winner(board)
     board[winning_combo.first]
   end
 end
-# Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,4,8],
-  [2,4,6],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8]
-]
